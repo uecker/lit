7797411c3718dc7cca07e472e12767ed799bbd12
Martin Uecker <martin.uecker@med.uni-goettingen.de>
2018-08-10 17:43:40 +0200

    add test.sh

---
+++ test.sh
@@ -0,0 +1,20 @@
+: : ex: set ft=markdown :<<'```shell' # set file format to markown
+
+## Demo
+
+Idea from: https://gist.github.com/pjeby/c137ace4d91e61e8f1f80e92d84e8b70
+
+
+```shell
+
+bart phantom x
+bart toimg x img
+
+:<<'```shell' # ignore this
+```
+
+## Result
+![img](img.png)
+
+```shell
+
